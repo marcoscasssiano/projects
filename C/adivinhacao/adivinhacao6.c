b061f08c086f0cb7768f585a4eb11c9191553ab8
Marcos Cassiano - Via Consulting <marcoscassiano@outlook.com.br>
2023-08-23 18:13:34 -0300

    add C exercicies

---
+++ C/adivinhacao/adivinhacao6.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+int main() {
+
+    // imprime cabecalho do nosso jogo
+    printf("******************************************\n");
+    printf("* Bem vindo ao nosso jogo de adivinhação *\n");
+    printf("******************************************\n");
+
+    int numerosecreto = 42;
+
+    int chute;
+
+    printf("chute %d\n", chute);
+
+    int tentativas = 1;
+
+    double pontos = 1000;
+    
+
+    while(1) {
+
+        printf("Tentativa %d\n", tentativas);
+        printf("Qual é o seu chute? ");
+        printf("Seu chute foi %d\n", chute);
+
+        if(chute < 0) {
+            printf("Você não pode chutar números negativos!\n");
+            continue;
+        }
+
+        int acertou = (chute == numerosecreto);
+        int maior = chute > numerosecreto;
+
+        if(acertou) {
+            printf("Parabéns! Você acertou!\n");
+            printf("Jogue de novo, você é um bom jogador!\n");
+
+            break;
+        }
+
+        else if(maior) {
+            printf("Seu chute foi maior que o número secreto\n");
+        } 
+
+        else {
+            printf("Seu chute foi menor que o número secreto\n");
+        }
+
+        tentativas++;
+
+        double pontosperdidos = (chute - numerosecreto) / (double)2;
+        pontos = pontos - pontosperdidos;
+    }
+
+    printf("Fim de jogo!\n");
+    printf("Você acertou em %d tentativas!", tentativas);
+    printf("total de pontos: %.1f\n", pontos);
+}
